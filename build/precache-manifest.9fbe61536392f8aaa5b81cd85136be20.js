486ceadd4f0aac1379e7cb1e0543641380af12b5
Dave Ward <professorx00@gmail.com>
2019-11-06 15:00:14 -0500

    Build Run

---
+++ build/precache-manifest.9fbe61536392f8aaa5b81cd85136be20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7a877205fd1aeeb9d1e400ea152806e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b14852be8c44df3ffe21",
+    "url": "/static/css/main.9fbbe7dd.chunk.css"
+  },
+  {
+    "revision": "dcf649ae6d674c22be6d",
+    "url": "/static/js/2.8e8cdedd.chunk.js"
+  },
+  {
+    "revision": "b14852be8c44df3ffe21",
+    "url": "/static/js/main.d6fd90c9.chunk.js"
+  },
+  {
+    "revision": "afa2fc5fd129db8e631a",
+    "url": "/static/js/runtime-main.1285a846.js"
+  }
+]);
